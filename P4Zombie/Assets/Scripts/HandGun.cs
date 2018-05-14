bbcf325f2a35921c532bd244a867dc09b85ac3b3
JorritKornelis <33859036+JorritKornelis@users.noreply.github.com>
2018-05-14 14:54:00 +0200

    Script
    move en wapeonbase

---
+++ P4Zombie/Assets/Scripts/HandGun.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandGun : WeaponBase
+{
+
+
+	void Start ()
+    {
+		
+	}
+
+    private void Update()
+    {
+        Shoot();
+    }
+
+    public override void Shoot()
+    {
+        Debug.DrawRay(transform.position, transform.forward * 5, Color.red);
+
+    }
+}
