66f6c8ec7bd430d8c655a4c892ca2ff5f415e9b8
Mas Tijben <Whosobvious@gmail.com>
2018-05-14 13:05:20 +0200

    added looking around
    still need to clamp

---
+++ P4Zombie/Assets/Scripts/LookAround.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAround : MonoBehaviour
+{
+    public float sensitivity;
+    Vector3 xRotation;
+    Vector3 yRotation;
+    public GameObject cameraY;
+
+    void Update ()
+    {
+        Look();
+	}
+
+    void Look()
+    {
+        xRotation.x = -Input.GetAxis("Mouse Y");
+        yRotation.y = Input.GetAxis("Mouse X");
+
+        transform.Rotate(xRotation * (sensitivity * 10) * Time.deltaTime);
+
+        cameraY.transform.Rotate(yRotation * (sensitivity * 10) * Time.deltaTime);
+    }
+}
