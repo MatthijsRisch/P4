1427d471c9bd50b9a61b4522645497dfb910c265
JorritKornelis <33859036+JorritKornelis@users.noreply.github.com>
2018-05-28 13:47:31 +0200

    TussenPuch
    Bezig met bod spawner

---
+++ P4Zombie/Assets/Scripts/Enemies/MobSpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobSpawner : MonoBehaviour
+{
+    public GameObject zombie;
+    public int spawTime;
+    bool spawnCheck;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (spawnCheck == true)
+        {
+            StartCoroutine(Spawner());
+            spawnCheck = false;
+        }
+    }
+
+    IEnumerator Spawner()
+    {
+        yield return new WaitForSeconds(spawTime);
+        Instantiate(zombie, transform.position, transform.rotation);
+        spawnCheck = true;
+    }
+}
